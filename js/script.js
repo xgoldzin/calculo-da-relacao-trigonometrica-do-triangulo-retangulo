007a3b2a86d72dcb6d5ff7ea34afa26ea1b8465e
xgoldzin <gustafera34@gmail.com>
2024-09-24 17:07:37 -0300

    last commit

--- js/script.js
+++ js/script.js
@@ -11,5 +11,19 @@ function calcTrig(){
     document.getElementById("paragraftang").innerText = `Tangente: ${tang}`
 }
 
+function calcVolBsk(){
+    let valorA =  Number(document.getElementById("vlrA").value)
+    let valorB =  Number(document.getElementById("vlrB").value)
+    let valorC =  Number(document.getElementById("vlrC").value)
+    const delta = ( (valorB ** 2) - 4 * valorA * valorC)
+    const x1 = (((-1 *(valorB)) - (delta ** 0.5)) / (2 * valorA))
+    const x2 =(((-1 *(valorB)) + (delta ** 0.5)) / (2 * valorA))
+
+    document.getElementById("numx1").innerText = "X1: "+ x1
+    document.getElementById("numx2").innerText = "X2: "+ x2
+    document.getElementById("dtt").innerText = "Delta: "+ delta
+}
+
+
 
 
